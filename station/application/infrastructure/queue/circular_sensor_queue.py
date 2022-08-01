93424eafcc8a237c52fe4b0297529dc33f2eca1b
bit5 <tec.cmos@gmail.com>
2022-07-31 22:41:15 -0500

    added documentation to circular queue

--- station/application/infrastructure/queue/circular_sensor_queue.py
+++ station/application/infrastructure/queue/circular_sensor_queue.py
@@ -4,6 +4,10 @@ from application.utils.decorators.synchronized import synchronized
 
 
 class CircularSensorQueue(SensorQueue):
+    """
+     An implementation of SensorQueue.
+     When the queue is full, the older data will be overridden by most recent data
+    """
 
     def __init__(self, capacity=100):
         self.capacity = capacity
@@ -13,11 +17,24 @@ class CircularSensorQueue(SensorQueue):
 
     @synchronized
     def put(self, value: SensorData):
+        """
+        puts a new entry in the queue. If the queue is full, then the new record will
+        be store in the beginning of the queue. This means, older data will be overridden by most
+        recent data if it was not consumed in time
+
+        :param value: the new data entry
+        """
         self.queue[self.write_pointer] = value
         self.write_pointer = (self.write_pointer + 1) % self.capacity
 
     @synchronized
     def get(self) -> SensorData:
+        """
+        reads the value from the queue.
+
+        @return the sensor data
+        @raise EmptySensorQueueException if the queue is empty
+        """
         if self.is_empty():
             raise EmptySensorQueueException
 
@@ -27,9 +44,19 @@ class CircularSensorQueue(SensorQueue):
         return read_value
 
     def is_empty(self) -> bool:
+        """
+        checks if the queue is empty.
+
+        :returns True if the queue is empty, False otherwise
+        """
         return self.read_pointer == self.write_pointer
 
     def is_full(self) -> bool:
+        """
+        checks if the queue is full.
+
+        :returns True if the queue is full, False otherwise
+        """
         return self.read_pointer == self.write_pointer
 
 
