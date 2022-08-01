5e22c1b0edea619131d4f2c346d3aa6f2cf47373
bit5 <tec.cmos@gmail.com>
2022-07-31 23:27:19 -0500

    created fake mqqt sender

--- station/application/infrastructure/mqtt/sender/fake_mqtt_sender.py
+++ station/application/infrastructure/mqtt/sender/fake_mqtt_sender.py
@@ -2,9 +2,17 @@ from abc import abstractmethod
 
 from application.domain.sensor.sender.sensor_sender import SensorSender
 from application.domain.sensor.sensor import SensorData
+from application.infrastructure.mqtt.config.mqtt_config import MqttConfig
 
 
 class FakeMqttSender(SensorSender):
+    """
+     A fake implementation of the SensorSender interface which.... does nothing
+     (for testing purposes)
+    """
+
+    def __init__(self, mqtt_config: MqttConfig):
+        self.mqtt_config = mqtt_config
 
     def send(self, sensor_data: SensorData):
         print('sent sensor data via FAKE MQTT')
