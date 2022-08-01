76da120367ed5a1b6a9fc593e68282833d525649
bit5 <tec.cmos@gmail.com>
2022-07-31 23:32:42 -0500

    added config classes

---
+++ station/application/infrastructure/app/config/application_config.py
@@ -0,0 +1,106 @@
+import yaml
+
+
+class AppConfig:
+    """
+      A class that holds the config values for this application like the station id,
+      the mqtt endpoint, sensor configurations, etc.
+
+      it reads the provided yml file and creates a config instance holding the values defined in the yml file
+    """
+
+    def __init__(self, filename):
+        with open(filename, "r") as stream:
+            try:
+                self.configs = yaml.safe_load(stream)
+            except yaml.YAMLError as exception:
+                print("could not load the configuration file")
+                raise exception
+
+    @property
+    def station_id(self):
+        return self.configs['station']['id']
+
+    @property
+    def mqtt_host(self):
+        return self.configs['mqtt']['host']
+
+    @property
+    def mqtt_port(self):
+        return self.configs['mqtt']['port']
+
+    @property
+    def mqtt_keepalive(self):
+        return self.configs['mqtt']['keepalive']
+
+    @property
+    def temperature_mqtt_topic(self):
+        return self.configs['temperature']['mqtt_topic']
+
+    @property
+    def temperature_producer_time(self):
+        return self.configs['temperature']['producer_time']
+
+    @property
+    def temperature_consumer_time(self):
+        return self.configs['temperature']['consumer_time']
+
+    @property
+    def humidity_mqtt_topic(self):
+        return self.configs['humidity']['mqtt_topic']
+
+    @property
+    def humidity_producer_time(self):
+        return self.configs['humidity']['producer_time']
+
+    @property
+    def humidity_consumer_time(self):
+        return self.configs['humidity']['consumer_time']
+
+    @property
+    def pm25_mqtt_topic(self):
+        return self.configs['pm25']['mqtt_topic']
+
+    @property
+    def pm25_producer_time(self):
+        return self.configs['pm25']['producer_time']
+
+    @property
+    def pm25_consumer_time(self):
+        return self.configs['pm25']['consumer_time']
+
+    @property
+    def pm10_mqtt_topic(self):
+        return self.configs['pm10']['mqtt_topic']
+
+    @property
+    def pm10_producer_time(self):
+        return self.configs['pm10']['producer_time']
+
+    @property
+    def pm10_consumer_time(self):
+        return self.configs['pm10']['consumer_time']
+
+    @property
+    def co2_mqtt_topic(self):
+        return self.configs['co2']['mqtt_topic']
+
+    @property
+    def co2_producer_time(self):
+        return self.configs['co2']['producer_time']
+
+    @property
+    def co2_consumer_time(self):
+        return self.configs['co2']['consumer_time']
+
+    @property
+    def vocs_mqtt_topic(self):
+        return self.configs['vocs']['mqtt_topic']
+
+    @property
+    def vocs_producer_time(self):
+        return self.configs['vocs']['producer_time']
+
+    @property
+    def vocs_consumer_time(self):
+        return self.configs['vocs']['consumer_time']
