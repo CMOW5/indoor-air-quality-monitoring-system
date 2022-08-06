7f7ce88d94c1b1a86d2bbdf8356f0719bc422135
bit5 <tec.cmos@gmail.com>
2022-08-06 13:47:45 -0500

    initial project setup for iaq API

---
+++ iaq-api/src/test/java/com/cmow5/iaqapi/IaqApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmow5.iaqapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IaqApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
