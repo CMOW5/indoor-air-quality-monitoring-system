c2242cd56d02c452b3345155eba909da55f35ea9
bit5 <tec.cmos@gmail.com>
2022-08-13 15:54:22 -0500

    added aws-amplify (working) to connect to iot-core. Disabled for now

--- iaq-ui/src/app/core/mqtt/mqtt-subscriber.service.ts
+++ iaq-ui/src/app/core/mqtt/mqtt-subscriber.service.ts
@@ -1,52 +1,68 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { IMqttMessage, MqttService } from "ngx-mqtt";
 import { DataPoint } from './datapoint.interface';
+import { Amplify } from 'aws-amplify';
+import { AWSIoTProvider } from '@aws-amplify/pubsub/lib/Providers';
+import { MqttOverWSProvider } from "@aws-amplify/pubsub/lib/Providers";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MqttSubscriberService {
 
-  //private topicUrl = 'sensor/temperature';
   private topicUrl = 'sensor';
 
-  constructor(private _mqttService: MqttService) {}
-
-  createSensorSubscription(sensorId: string): Observable<IMqttMessage> {     
-    return new Observable((observer) => {
-      
-      this._mqttService.observe(this.topicUrl).subscribe((message: IMqttMessage) => {
-        const tempDataPoint = JSON.parse(message.payload.toString())
-        if (this.isDataForStationId(sensorId, tempDataPoint)) {
-          observer.next(tempDataPoint);
-        }
-      }); 
-      
-      // When the consumer unsubscribes, clean up data ready for next subscription.
-      return () => {
-        console.log('closing mqtt connection');
-        this._mqttService.disconnect();
-      };
+  constructor() {
+    /*
+    Amplify.configure({
+      Auth: {
+        identityPoolId: '<identity pool id>',
+        region: 'us-east-1',
+        //userPoolId: APP_USER_POOL_ID,
+        //userPoolWebClientId: APP_USER_POOL_WEB_CLIENT_ID
+      }
     });
+    */
+
+    const awsIotEndpoint = 'aws-iot-endpoint'
+
+    // AWS IoT Provider
+    /*
+    Amplify.addPluggable(new AWSIoTProvider({
+      aws_pubsub_region: 'us-east-1',
+      aws_pubsub_endpoint: `wss://${awsIotEndpoint}/mqtt`,
+    }));
+    */
+
+    // Third party MqttProvider
+    
+    Amplify.addPluggable(new MqttOverWSProvider({
+    //aws_pubsub_endpoint: 'wss://localhost:9001/mqtt',
+    aws_pubsub_endpoint: 'ws://localhost:9001/mqtt',
+    // disable SSL
+    aws_appsync_dangerously_connect_to_http_endpoint_for_testing: true
+    }));
   }
 
-  createSubscription(metric:string, stationId: string): Observable<DataPoint> {     
+  createSubscription(metric:string, stationId: string): Observable<DataPoint> {  
     return new Observable((observer) => {
       const topic = `${this.topicUrl}/${metric}`;
       console.log(`subscribed to topic ${topic}, for station = ${stationId}`);
-      const subscription = this._mqttService.observe(topic).subscribe((message: IMqttMessage) => {
-        const datapoint = JSON.parse(message.payload.toString())
-        if (this.isDataForStationId(stationId, datapoint)) {
-          observer.next(datapoint);
-        }
-      }); 
-      
+      const subscription = Amplify.PubSub.subscribe(topic).subscribe({
+        next: (data: MqttMessage) => {
+          const datapoint = data.value;
+          if (this.isDataForStationId(stationId, datapoint)) {
+            observer.next(datapoint);
+          }
+        },
+        error: (error: any) => console.error(error),
+        close: () => console.log('Done'),
+      });
+  
       // When the consumer unsubscribes, clean up data ready for next subscription.
       return () => {
         console.log('closing mqtt connection');
         subscription.unsubscribe();
-        //this._mqttService.disconnect();
       };
     });
   }
@@ -55,4 +71,8 @@ export class MqttSubscriberService {
   private isDataForStationId(stationId: String, datapoint: DataPoint): boolean {
     return datapoint.stationId === stationId;
   }
+}
+
+interface MqttMessage {
+  value: DataPoint
 }
