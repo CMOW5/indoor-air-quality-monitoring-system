fb7a6bc3e94d39cfaa82062903e1024db1269fcc
bit5 <tec.cmos@gmail.com>
2022-08-14 23:57:56 -0500

    now getting the line colors from backend

--- iaq-api/src/main/java/com/cmow5/iaqapi/domain/metric/MetricMetadata.java
+++ iaq-api/src/main/java/com/cmow5/iaqapi/domain/metric/MetricMetadata.java
@@ -4,18 +4,25 @@ public class MetricMetadata {
 
     private final String color;
 
+    private final String backgroundColor;
+
     private final String icon;
 
     private final int min;
 
-    public MetricMetadata(String color, String icon, int min) {
+    public MetricMetadata(String color, String backgroundColor, String icon, int min) {
         this.color = color;
+        this.backgroundColor = backgroundColor;
         this.icon = icon;
         this.min = min;
     }
 
     public String getColor() {
         return color;
+    }
+
+    public String getBackgroundColor() {
+        return backgroundColor;
     }
 
     public String getIcon() {
@@ -25,4 +32,5 @@ public class MetricMetadata {
     public int getMin() {
         return min;
     }
+
 }
