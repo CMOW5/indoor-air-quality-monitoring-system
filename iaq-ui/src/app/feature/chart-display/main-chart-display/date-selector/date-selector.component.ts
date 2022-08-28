f4b6d5137f3365914dc13dfb035275660a8100a7
bit5 <tec.cmos@gmail.com>
2022-08-28 18:15:50 -0500

    added form validation to realtime selector component

--- iaq-ui/src/app/feature/chart-display/main-chart-display/date-selector/date-selector.component.ts
+++ iaq-ui/src/app/feature/chart-display/main-chart-display/date-selector/date-selector.component.ts
@@ -1,6 +1,8 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { DateRange } from './date-range.interface';
+import { HistoricDateSelectorComponent } from './historic-date-selector/historic-date-selector.component';
+import { RealtimeDateSelectorComponent } from './realtime-date-selector/realtime-date-selector.component';
 
 @Component({
   selector: 'app-date-selector',
@@ -10,10 +12,17 @@ import { DateRange } from './date-range.interface';
 export class DateSelectorComponent implements OnInit {
 
   DEFAULT_DATE_TYPE = 'realtime';
- 
+  
+  // todo: type this
   dateTypes = ['realtime', 'historic'];
 
   dateTypeForm = new FormControl('', Validators.required);
+
+  @ViewChild('realtimeDateSelector')
+  private realtimeDateSelectorComponent!: RealtimeDateSelectorComponent;
+
+  @ViewChild('historicDateSelector')
+  private historicDateSelectorComponent!: HistoricDateSelectorComponent;
 
   @Output() timeSelected = new EventEmitter<DateRange>();
 
@@ -22,10 +31,18 @@ export class DateSelectorComponent implements OnInit {
   ngOnInit(): void {
     this.dateTypeForm.setValue(this.DEFAULT_DATE_TYPE);
   }
-
-
+  
+  
   onTimeSelected(dateRange: DateRange) {
-    this.timeSelected.emit(dateRange);
+    if (!this.isValid()) return;
+
+     this.timeSelected.emit(dateRange);
+  }
+
+  public isValid(): boolean {
+    return  this.dateTypeForm.valid && 
+            (this.dateTypeForm.value === 'realtime' ? this.realtimeDateSelectorComponent?.isValid : 
+                                                      this.historicDateSelectorComponent?.isValid);
   }
 
 }
