340508afdaa00cbadbdbac815d91713cd263e968
bit5 <tec.cmos@gmail.com>
2022-09-10 23:10:06 -0500

    added login (interceptors, guards, routes and services)

--- iaq-ui/src/app/app.module.ts
+++ iaq-ui/src/app/app.module.ts
@@ -1,9 +1,13 @@
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthGuard } from './core/auth/guard/auth.guard';
 import { CoreModule } from './core/core.module';
 import { FeatureModule } from './feature/feature.module';
+import { JwtInterceptor } from './feature/login/interceptors/jwt.interceptor';
+import { UnauthorizedInterceptor } from './feature/login/interceptors/unauthorized.interceptor';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
@@ -17,7 +21,11 @@ import { SharedModule } from './shared/shared.module';
     SharedModule,
     FeatureModule,
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor , multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: UnauthorizedInterceptor, multi: true },
+    AuthGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
